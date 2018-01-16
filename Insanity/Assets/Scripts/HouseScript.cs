97ddd5123b43e9790f999ea91a25129089c687b9
shotskeber <italo.pardave@gmail.com>
2018-01-16 09:27:15 +0100

    housetest

---
+++ Insanity/Assets/Scripts/HouseScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseScript : MonoBehaviour {
+
+	public GameObject exterior;
+	public GameObject interior;
+
+	bool canInteract = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (canInteract) {
+			if (Input.GetKeyDown (KeyCode.F)) {
+				exterior.SetActive (false);
+				interior.SetActive (true);
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		canInteract = true;
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		canInteract = false;
+	}
+}
