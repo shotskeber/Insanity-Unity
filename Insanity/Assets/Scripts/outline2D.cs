730fff1331208f5fd97af9c6ebd7be30a07a50b7
shotskeber <italo.pardave@gmail.com>
2018-01-31 17:58:54 +0100

    shading

---
+++ Insanity/Assets/Scripts/outline2D.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class outline2D : MonoBehaviour {
+	
+		public Color color = Color.white;
+
+		[Range(0, 16)]
+		public int outlineSize = 1;
+		public bool outlineBorderNotInternal = true; 
+
+		private SpriteRenderer spriteRenderer;
+
+		void OnEnable()
+		{
+			spriteRenderer = GetComponent<SpriteRenderer>();
+
+			UpdateOutline(true);
+		}
+
+		void OnDisable()
+		{
+			UpdateOutline(false);
+		}
+
+		void Update()
+		{
+			UpdateOutline(true);
+		}
+
+		void UpdateOutline(bool outline)
+		{
+			MaterialPropertyBlock mpb = new MaterialPropertyBlock();
+			spriteRenderer.GetPropertyBlock(mpb);
+			mpb.SetFloat("_Outline", outline ? 1f : 0);
+			mpb.SetColor("_OutlineColor", color);
+			mpb.SetFloat("_OutlineSize", outlineSize);
+			mpb.SetFloat("_OutlineBorderNotInternal", outlineBorderNotInternal ? 1 : 0); 
+			spriteRenderer.SetPropertyBlock(mpb);
+		}
+
+}
